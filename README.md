399355ec7d0878970bf3f988f04446e1ee2893a3
MaG <maguilamo.c@gmail.com>
2013-05-20 01:25:48 -0430

    upload

---
+++ README.md
@@ -0,0 +1,20 @@
+Alpha channel
+==
+For years steganography has been used in many circumstances by a lot of people
+around the world, each of them with different goals, but with only one reason,
+to hide the information. Today steganography could be say to be not quite a
+popular subject but yet it's a very advance field with scary algorithms and
+very advance methods; information can be hidden almost everywhere.
+
+This program can be used to introduce information inside an image's alpha
+channel. Although, it has several limitations, for instance, one cannot use
+binary data or another image format apart from PNG with this program.
+
+This program depends on, the chunky_png gem.
+	gem install chunky_png
+
+License
+-------
+This content is released under the terms of the MIT License. For more
+information refer to file COPYRIGHT included in this distribution.
+
---
+++ alpha-steg.rb
@@ -0,0 +1,248 @@
+#!/usr/bin/ruby
+# encoding: UTF-8
+# Author: MaG
+# URL: http://newbieshell.blogspot.com
+
+require 'chunky_png'
+require 'getoptlong'
+
+class IO
+	def usage
+		self.puts DATA.read()
+	end
+end
+
+def get_available_size(image)
+	size = 0
+	for i in 0...image.width
+		for j in 0...image.height
+			size += 3 if ChunkyPNG::Color.a(image[i,j]).zero?
+		end
+	end
+	size
+end
+
+def clean_image(image)
+	for i in 0...image.width
+		for j in 0...image.height
+			image[i,j] = 0x00000000 if ChunkyPNG::Color.a(image[i,j]).zero?
+		end
+	end
+end
+
+def show_alpha(image)
+	for i in 0...image.width
+		for j in 0...image.height
+			value = image[i,j]
+			if ChunkyPNG::Color.a(value).zero?
+				image[i,j] = value|0xFF
+			end
+		end
+	end
+end
+
+def unhide_text(img)
+	text = ''.force_encoding('binary')
+	chars = []
+	for i in 0...img.width
+		for j in 0...img.height
+			num = img[i,j]
+			if ChunkyPNG::Color.a(num).zero?
+				chars[0] = ChunkyPNG::Color.r(num).chr
+				chars[1] = ChunkyPNG::Color.g(num).chr
+				chars[2] = ChunkyPNG::Color.b(num).chr
+
+				chars.each do|c|
+					return text if c.ord.zero?
+					text << c
+				end
+			end
+		end
+	end
+	text
+end
+
+def hide_text(img, text)
+	clean_image(img)
+
+	interval = text.length - (text.length%3)
+	left = text.length - interval
+
+	idx = 0
+	for i in 0...img.width
+		for j in 0...img.height
+			return idx if idx > text.length-1
+
+			next if ChunkyPNG::Color.a(img[i,j]).nonzero?
+
+			if idx == interval
+				if left == 2
+					img[i,j] = ChunkyPNG::Color.rgba(text[idx].ord, text[idx+1].ord, 0x00, 0x00)
+				else
+					img[i,j] = ChunkyPNG::Color.rgba(text[idx].ord, 0x00, 0x00, 0x00)
+				end
+				return idx + left
+			end
+
+			img[i,j] = ChunkyPNG::Color.rgba(text[idx].ord, text[idx+1].ord, text[idx+2].ord, 0x00)
+			idx += 3
+		end
+	end
+	idx
+end
+
+opts = GetoptLong.new(
+	['--help', '-h', GetoptLong::NO_ARGUMENT],
+	['--clear-image', GetoptLong::NO_ARGUMENT],
+	['--free-space', GetoptLong::NO_ARGUMENT],
+	['--unhide', '-u', GetoptLong::NO_ARGUMENT],
+	['--show-alpha', GetoptLong::NO_ARGUMENT],
+	['--output-file', '-o', GetoptLong::REQUIRED_ARGUMENT],
+	['--text', '-t', GetoptLong::REQUIRED_ARGUMENT],
+	['--file', '-f', GetoptLong::REQUIRED_ARGUMENT],
+	['--png-image', GetoptLong::REQUIRED_ARGUMENT])
+
+var = {}
+
+opts.quiet = true
+begin
+	opts.each do |op, arg|
+		case op
+		when '--help'
+			$stdout.usage()
+			exit
+		when '--text'
+			var[:text] = arg
+		when '--file'
+			var[:filename] = arg
+		when '--png-image'
+			var[:png_image] = arg
+		when '--unhide'
+			var[:unhide] = true
+		when '--clear-image'
+			var[:clear] = true
+		when '--free-space'
+			var[:space] = true
+		when '--output-file'
+			var[:output] = arg
+		when '--show-alpha'
+			var[:show_alpha] = true
+		end
+	end
+rescue GetoptLong::MissingArgument
+	$stderr.puts 'Option requires an argument.'
+	exit
+rescue GetoptLong::InvalidOption
+	$stderr.puts 'Unknown option.'
+	$stderr.usage
+	exit
+rescue
+	puts $!
+	exit
+end
+
+unless var[:png_image] 
+	$stderr.puts '--> Please, provide an image to work with.'
+	$stderr.usage
+	exit
+end
+
+begin
+	if not File.exists? var[:png_image] or var[:png_image] =~ /\.png$/i
+		$stderr.puts "--> Image not found or it is not a *.png image `#{var[:png_image]}'"
+		exit
+	end
+	img = ChunkyPNG::Image.from_file(var[:png_image])
+rescue
+	puts $!
+	exit
+end
+
+if var[:space] and var[:clear]
+	puts 'Cleaning image...'
+	clean_image(img)
+	puts 'Getting available space...'
+	size = get_available_size(img)
+	puts "\n#{size} bytes available."
+	img.save(var[:png_image])
+	exit
+
+elsif var[:clear]
+	print 'Cleaning the image...'
+	clean_image(img)
+	img.save(var[:png_image])
+	puts 'Done.'
+	exit
+
+elsif var[:space]
+	puts 'Getting available space...'
+	size = get_available_size(img)
+	puts "\n#{size} bytes available."
+	exit
+
+elsif var[:show_alpha]
+	if var[:output]
+		var[:output] += '.png' unless var[:output] =~ /\.png$/i
+
+		print 'Changing bits...'
+		show_alpha(img)
+
+		puts "Done.\nSaving changes to `#{var[:output]}'..."
+		img.save(var[:output])
+		
+	else
+		$stderr.print '--> You must use the `--show-alpha\' function in conjunction with'
+		$stderr.puts 'the `--output-file\' option.'
+	end
+	exit
+elsif var[:unhide]
+	payload = unhide_text(img)
+	if var[:output] and payload!=''
+		f = File.new(var[:output], 'w')
+		f.write(payload)
+		f.close
+	else
+		puts payload
+	end
+
+	puts "\n#{payload.size} bytes read."
+	exit
+
+elsif var[:filename] or var[:text]
+	if var[:filename]
+		begin
+			var[:text] = IO.read(var[:filename])
+		rescue
+			puts "--> Problems reading the file `#{var[:filename]}'"
+			puts "--> #$!"
+			exit
+		end
+	end
+	len = hide_text(img, var[:text]);
+
+	print 'Saving changes...'
+	img.save(var[:png_image])
+	puts "\n#{len} bytes written."
+else
+	$stderr.puts '--> Please, provide an action.'
+	$stderr.usage
+	exit
+end
+
+__END__
+Autor: MaG
+Prove of concept program.
+
+Uso: script [OPTIONS]
+
+Optiones:
+  --help, -h         Display this help and exit.
+  --text, -t         Text to be hidden within the image.
+  --file, -f         File to be hidden within the image. (text files only)
+  --png-image        PNG image to work with.
+  --unhide, -u       Gather the hidden information inside the image.
+  --clear-image      Removes the information hidden within image.
+  --free-space       Show available space and exit.
+  --output-file, -o  Place output in a file.
+  --show-alpha       Reveals the alpha channel of the image.
+
